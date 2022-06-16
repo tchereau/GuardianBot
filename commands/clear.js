b0728cd635480c93a17a83f0e7dc699b83edf62c
tchereau <>
2022-06-17 01:53:21 +0200

    ✅ update

--- commands/clear.js
+++ commands/clear.js
@@ -9,6 +9,10 @@ export const clear = async (argsBody) => {
         return;
     }
     if(amount <= 100){
+        //si amount est inférieur ou égal à 99 alors on ajoute 1
+        if (amount <= 99){
+            amount = amount + 1;
+        }
         argsBody.message.channel.messages.fetch({limit: amount}).then( async messages => {
             await messages.forEach(async message => {
                 await message.delete();
